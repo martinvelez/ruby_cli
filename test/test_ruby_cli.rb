69bb3fbc690119be2248e1f52862b6c464c4af1c
Martin Velez <mvelez999@gmail.com>
2011-11-11 20:55:20 -0800

    continued working on tests

--- test/test_ruby_cli.rb
+++ test/test_ruby_cli.rb
@@ -2,5 +2,12 @@ require 'test/unit'
 require 'ruby_cli'
 
 class RubyCLITest < Test::Unit::TestCase
-  # TODO: Add tests
+  def test_hello_world
+		m = Class.new do
+			include RubyCLI	
+
+			def command()	puts "Hello World" end
+		end.new(ARGV)
+		assert_equal(0, m.run)
+  end
 end
