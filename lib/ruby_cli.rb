655f0a7603732806ed74f125df116f5ffdd7e84a
Martin Velez <mvelez999@gmail.com>
2011-11-11 19:47:06 -0800

    Factors out code needed to create Ruby command line applications

---
+++ lib/ruby_cli.rb
@@ -0,0 +1,121 @@
+require 'optparse'
+
+# The goal of this library is to factor out code needed to create Ruby 
+# command line applications which follow the Unix Philosophy design method 
+# (http://www.faqs.org/docs/artu/ch01s06.html).
+#
+# What does a command line application library need to do?
+#	1. Provide a UI
+#	a. Process options (use Ruby's Option Parser)
+# b. Process arguments
+# 2. Pass options and arguments as parameters to other functions defined in
+#	libraries or other executables.
+#
+# What does a command line application library need not do?
+# 1. A command line application does not need to validate options or arguments.
+# Libraries or other executables should do this.
+#
+# This module serves as a mixin for Ruby Command Line Applications (CLI).
+# Ruby commands can be written much easier by including this class and following
+# the convention that I have outlined here.
+#
+# This is the core algorithm of any Ruby CLI application.
+# def run
+#   if parse_options? && arguments_valid?
+#		process_options
+#     process_arguments
+#     output_options_and_arguments if @default_options[:verbose]
+#     command
+#   else
+#     output_help(1)
+#   end
+#  end
+module RubyCLI
+
+	# Initialization of this application requires the command line arguments.
+	def initialize(default_argv)
+		@default_argv = default_argv
+		@default_options = {:help => false, :verbose => false}
+		define_command_options
+		define_command_arguments
+		@opt_parser = nil
+	end
+
+	# This method can be overwritten if you want to set defaults for your command
+	# specific options.
+	def define_command_options() @options = {} end
+	
+	# This method can be overwritten if you want to set defaults for your command
+	# specific arguments.
+	def define_command_arguments() @arguments = {} end
+	
+	# Run the application
+  def run
+    if parse_options? && arguments_valid?
+			process_options
+      process_arguments
+      output_options_and_arguments if @default_options[:verbose]
+      command
+    else
+      output_help(1)
+    end
+  end
+		
+		
+	# Parse the options
+	# Redefine this method if you want to add command specific options
+	def parse_options?
+		#configure an OptionParser
+		@opt_parser = OptionParser.new do |opts|		
+			opts.banner = "Usage: #{__FILE__} [OPTIONS]... [ARGUMENTS]..."
+			opts.separator ""
+			opts.separator "Specific options:"
+			opts.on('-h', '--help', 'displays help information') do
+				@default_options[:help] = true
+				exit output_help(0)
+			end
+			opts.on('-V','--verbose','Run verbosely') do 
+				@default_options[:verbose] = true
+			end
+			# TODO: If you redefine, you can add command specific options here!
+		end
+		@opt_parser.parse!(@default_argv) rescue return false
+		true
+	end
+
+	# Check if the required number of arguments remains in the 
+	# argv array after it has been processed by the option parser
+	def arguments_valid?() 
+		return true if @arguments.size == 0
+		@default_argv.size == @arguments.size 
+	end
+
+	def output_options_and_arguments
+    puts "OPTIONS:"
+    @default_options.each {|name, value| puts "#{name} = #{value}"}
+    @options.each {|name, value| puts "#{name} = #{value}"}
+		puts "No options" if @options.length == 0
+		
+		puts "ARGUMENTS:"
+    @arguments.each {|name,value| puts "#{name} = #{value}"}
+		puts "No arguments" if @arguments.length == 0
+  end
+
+	# Performs post-parse processing on options
+	# For instance, some options may be mutually exclusive
+	# Redefine if you need to process options.
+	def process_options() true end	
+	
+	# Redefine if you need to process options.
+	def process_arguments() true end
+	
+	# Application logic
+	def command() raise "This method should be overwritten." end
+	
+	def output_help(exit_code) 
+		puts @opt_parser
+		puts exit_code if @default_options[:verbose]
+		return exit_code 
+	end
+	
+end # Application
---
+++ ruby_cli.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+	s.name = 'ruby_cli'
+	s.version = '0.0.1'
+	s.executables = ['hello_world']
+	s.date = '2011-11-11'
+	s.summary = "A command line application library"
+	s.description = "Factors out code needed to create Ruby command line applications"
+	s.authors = ["Martin Velez"]
+	s.email = 'mvelez999@gmail.com'
+	s.files = ["lib/ruby_cli.rb", "README.rdoc", "bin/hello_world"]
+	s.homepage = 'http://github.com/martinvelez/ruby_cli'
+	s.require_paths = ["lib"]
+end
